77dbaf8ca417401f2a7f7bd9a207ff4af935e624
MikolajZelazny <MikolajZelazny.it@gmail.com>
2021-01-07 22:54:20 +0100

    Server properties

---
+++ GameServer/Server.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace GameServer
+{
+    public class Server
+    {
+        public static int MaxPlayers { get; private set; }
+        
+        public static int Port { get; private set; };
+        
+        public static int TcpListener tcpListener;
+
+
+    }
+}
