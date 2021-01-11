891e2cb31c1a0bb8458871eef20f25d0809824ff
MikolajZelazny <MikolajZelazny.it@gmail.com>
2021-01-11 23:56:48 +0100

    Client class

---
+++ GameServer/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameServer
+{
+    public class Client
+    {
+        public int id;
+        public TCP tcp;
+
+        public Client(int _clientId)
+        {
+            id = _clientId;
+            tcp = new TCP();
+        }
+
+        public class TCP
+        {
+            public TcpClient socket;
+
+            private readonly int id;
+        }
+    }
+
+}
